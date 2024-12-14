59c5989fef644f011c1207fbc0aa7aaa8c21bb04
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-13 21:23:03 -0300

    atualizado

--- java.js
+++ java.js
@@ -1,11 +1,23 @@
 
-function fun() {
-    document.getElementById("ap").innerHTML = 'carlos alberto soares nascimento'
-    
-};
+var myIndex = 0;
+carousel();
 
 
+function carousel(){
+    var i;
+    var x;
+    document.getElementsByClassName("myslides");
+    for(i = 0; i < x.length; i++){
+        x[1].style.display = "none";
+    }
+    myIndex++;
+    if(myIndex > x.length){
+        myIndex = 1
+    }
 
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel,2000);
+}
 
 
 
