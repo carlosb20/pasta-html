32fe81838ca743bd823e400e60cffd786c694963
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-15 23:08:42 -0300

    a

--- java.js
+++ java.js
@@ -16,7 +16,26 @@ function func(){
 const num = [...document.getElementsByClassName("item")]
 
 
+var myIdex = 0;
+carousel();
 
+function carousel(){
+
+    var i;
+    var x = document.getElementsByClassName("mySlides")
+    
+    for (i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+        
+    } 
+    myIdex ++;
+    if (myIdex > x.length){
+
+        myIdex = 1
+    }
+    x[myIdex-1].style.display = "block";
+    setTimeout(carousel,6000);
+}
 
 
 
