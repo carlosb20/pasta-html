878bb04ca10bbcc3e5494394cff2696015e5a2b1
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-20 00:23:11 -0300

    atualizado

--- java.js
+++ java.js
@@ -14,22 +14,45 @@ var myIdex = 0;
 carousel();
 
 function carousel(){
-
     var i;
     var x = document.getElementsByClassName("mySlides")
-    
     for (i = 0; i < x.length; i++){
-        x[i].style.display = "none";
-       
+        x[i].style.display = "none";  
     } 
     myIdex ++;
     if (myIdex > x.length){
-
         myIdex = 1
     }
     x[myIdex-1].style.display = "block";
     setTimeout(carousel,6000);
 }
+
+// -------------------------------------------------------------------
+
+window.addEventListener("load",function(){
+
+    setInterval(function(){
+
+        changeBgColor();
+    } ,1000)
+});
+
+function changeBgColor(){
+    function random(number){
+        return Math.floor(Math.random() * (number+1));
+    }
+    const mundaCor = `rgb(${random(255)},${random(255)},${random(255)})`;
+    const num = document.querySelector("#div_4")
+    num.style.backgroundColor = mundaCor  
+}
+
+
+
+
+
+
+
+
 
 
 
