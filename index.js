9b423947eacee384061069b02718b93c35661a50
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-20 23:44:18 -0300

    a

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function num(){
+
+    var val = document.querySelector(".item1").classList;
+    val.add('myStyle')
+
+    const ca = document.querySelector(".item1")
+    ca.innerHTML = 'carlos'
+    
+    console.log(val)
+}
+
+
+
+
+
+
+
+
+
