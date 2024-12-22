a1559c07036c1326b65567d7e646565b08ba7946
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-22 00:25:14 -0300

    a

--- index.js
+++ index.js
@@ -12,10 +12,37 @@ function num(){
 }
 
 
+const url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL';
 
+const divItem = document.querySelector("#cre")
 
+async function getPosta(){
 
+    const response = await fetch(url);
+    const data = await response.json();
 
+    const tp = document.createElement("p")
+    divItem.appendChild(tp)
 
+    tp.innerHTML = data['USDBRL'].bid + " $ ";
+    tp.style.backgroundColor = 'green';
+    tp.style.border = '1px solid red';
+    tp.style.color = 'white'
+    tp.style.textAlign = 'center'
+    tp.style.borderRadius = '10px'
+   
+}
 
+getPosta()
 
+const fil = 'https://www.themoviedb.org/';
+
+async function filmes(){
+    
+    const filme = await fetch(fil);
+    const ver = await filme.json()
+
+    console.log(ver)
+}
+
+filmes()
