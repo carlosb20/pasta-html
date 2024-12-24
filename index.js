f517ae4696bd399c3cb67639fad47a25ebed998b
carlos alberto <carlosalbertosnb20@gmail.com>
2024-12-24 01:20:43 -0300

    atualizado

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function num(){
     
 }
 
+//-------------------------------------------------------------------------------
 
 const url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL';
 
@@ -30,11 +31,13 @@ async function getPosta(){
     tp.style.color = 'white'
     tp.style.textAlign = 'center'
     tp.style.borderRadius = '10px'
+    
    
 }
 
 getPosta()
 
+//------------------------------------------------------------------------------------
 
 const btn = document.createElement("button");
 btn.style.backgroundColor = 'orange'
@@ -51,7 +54,6 @@ btn.addEventListener('mouseenter',()=>{
 
 btn.addEventListener('mouseleave',()=>{
 
-   
     btn.style.transform = 'scale(1.0)'
     
 })
@@ -60,13 +62,59 @@ const div_item = document.querySelector(".item2")
 
 div_item.appendChild(btn)
 
+// --------------------------------------------------------------------------------
+
+const valor = [...document.getElementsByClassName("item3")]
 
 
 
 
+valor.map((el) => {
+
+    const p = document.createElement('p')
+    p.style.fontSize = '40px'
+    p.style.textShadow = '1px 1px red'
+    p.innerHTML = 'carlos alberto soares nascimento'
+    
+    const das = document.querySelector("#pos1")
+    das.style.borderRadius = '10px'
+    das.innerHTML = 'div id = pos1'
+    das.style.backgroundColor = 'pink'
+    das.style.border = '1px solid green'
+    das.style.textAlign = 'center'
+    
+    const pos2 = document.querySelector("#pos2")
+    pos2.style.backgroundColor = 'orange'
+    pos2.innerHTML = 'div id pos2'
+    pos2.style.borderRadius = '10px'
+
+    const pos3 = document.querySelector("#pos3")
+    pos3.style.backgroundColor = '#00bfff'
+    pos3.innerHTML = 'div id pos2'
+    pos3.style.borderRadius = '10px'
+
+    const pos4 = document.querySelector("#pos4")
+    pos4.style.backgroundColor = 'green'
+    pos4.innerHTML = 'div id pos2'
+    pos4.style.borderRadius = '10px'
+    
+    das.appendChild(p)
+
+})
+
+// cb9799f1bfc9e4663813cd632bccb561
+
+const tem = "http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=parauapebas&state=PA&token=cb9799f1bfc9e4663813cd632bccb561"
 
 
+async function tempo(){
 
+    const re = await fetch(tem);
+    const Tempo = await re.json();
+    console.log(Tempo)
+}
+
+tempo()
 
 
 
